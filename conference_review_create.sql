d7dd78f171c32a4acef5a5e886359b34fe045204
mehertech55 <46745147+mehertech55@users.noreply.github.com>
2020-05-17 19:55:05 -0400

    Update conference_review_create.sql

--- conference_review_create.sql
+++ conference_review_create.sql
@@ -23,7 +23,6 @@ DROP TABLE IF EXISTS `Author`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Author` (
-  `id` int NOT NULL AUTO_INCREMENT,
   `emailAdd` varchar(30) NOT NULL,
   `firstName` varchar(30) DEFAULT NULL,
   `lastName` varchar(30) DEFAULT NULL,
