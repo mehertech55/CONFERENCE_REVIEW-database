b90c5c6ce49dc1d3addc2278d1dbebcb84b08f08
mehertech55 <46745147+mehertech55@users.noreply.github.com>
2020-05-17 19:50:03 -0400

    Update conference_review_create.sql

--- conference_review_create.sql
+++ conference_review_create.sql
@@ -23,10 +23,11 @@ DROP TABLE IF EXISTS `Author`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Author` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `emailAdd` varchar(30) NOT NULL,
   `firstName` varchar(30) DEFAULT NULL,
   `lastName` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`emailAdd`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
