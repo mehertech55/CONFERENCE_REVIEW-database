8d914a1e0c5686a5473a9c0074637dc5fc4e19f1
mehertech55 <46745147+mehertech55@users.noreply.github.com>
2020-05-17 20:12:23 -0400

    Update conference_review_create.sql

--- conference_review_create.sql
+++ conference_review_create.sql
@@ -26,7 +26,7 @@ CREATE TABLE `Author` (
   `emailAdd` varchar(30) NOT NULL,
   `firstName` varchar(30) DEFAULT NULL,
   `lastName` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`emailAdd`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
